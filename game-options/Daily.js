6c47625d2742f2da638013860d09ffe87985d654
JeffVeron <67024868+JeffVeron@users.noreply.github.com>
2023-08-24 11:45:54 +0000

    Add files via upload

---
+++ game-options/Daily.js
@@ -0,0 +1,13 @@
+// Daily.js
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Daily = () => {
+  return (
+    <View>
+      <Text>Daily Component</Text>
+    </View>
+  );
+};
+
+export default Daily;
---
+++ game-options/Help.js
@@ -0,0 +1,13 @@
+ 
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Help = () => {
+  return (
+    <View>
+      <Text>Help Component</Text>
+    </View>
+  );
+};
+
+export default Help;
